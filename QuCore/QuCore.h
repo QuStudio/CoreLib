17a36886782b8149458c7b3dd72fb86b77401de4
Oleg Dreyman <dreymonde@me.com>
2016-03-21 18:44:02 +0200

    Initial Commit

---
+++ QuCore/QuCore.h
@@ -0,0 +1,19 @@
+//
+//  QuCore.h
+//  QuCore
+//
+//  Created by Oleg Dreyman on 21.03.16.
+//  Copyright © 2016 Oleg Dreyman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QuCore.
+FOUNDATION_EXPORT double QuCoreVersionNumber;
+
+//! Project version string for QuCore.
+FOUNDATION_EXPORT const unsigned char QuCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuCore/PublicHeader.h>
+
+
