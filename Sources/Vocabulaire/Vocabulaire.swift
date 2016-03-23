39c1e09a554dc4705ff931ec93ec6863ccbcb538
Oleg Dreyman <dreymonde@me.com>
2016-03-23 14:05:47 +0200

    Vocabulaire typealias

---
+++ Sources/Vocabulaire/Vocabulaire.swift
@@ -0,0 +1,11 @@
+//
+//  Vocabulaire.swift
+//  Vocabulaire
+//
+//  Created by Oleg Dreyman on 23.03.16.
+//  Copyright © 2016 Oleg Dreyman. All rights reserved.
+//
+
+import Foundation
+
+public typealias Vocabulaire = [Entry]
