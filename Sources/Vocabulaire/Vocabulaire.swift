1d2ceef4379f72f1f4fc8e1c8881460c6b053065
Oleg Dreyman <dreymonde@me.com>
2016-03-23 16:25:45 +0200

    Alphabetical sort fixed

--- Sources/Vocabulaire/Vocabulaire.swift
+++ Sources/Vocabulaire/Vocabulaire.swift
@@ -15,7 +15,7 @@ public typealias Vocabulary = [Entry]
 extension SequenceType where Generator.Element == Entry {
 
 	var alphabetical: Vocabulary {
-		return self.sort({ $0 < $1 })
+		return self.sort({ $0.foreign.lemma.view < $1.foreign.lemma.view })
 	}
 
 }
