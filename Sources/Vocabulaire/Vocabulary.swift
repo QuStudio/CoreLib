e736c5d1c3d3125a52a8aa66c5c37f51eb594410
Oleg Dreyman <dreymonde@me.com>
2016-04-30 19:38:44 +0300

    Vocabulary struct

--- Sources/Vocabulaire/Vocabulary.swift
+++ Sources/Vocabulaire/Vocabulary.swift
@@ -7,26 +7,63 @@
 //
 
 /// An array of entrues.
-public typealias Vocabulary = [Entry]
+public typealias Vocabularee = [Entry]
 
-#if swift(>=3.0)
-extension Sequence where Iterator.Element == Entry {
-    
-    /// Entries sorted in alphabetical order.
-    var alphabetical: Vocabulary {
-        return self.sorted { $0.foreign.lemma.view < $1.foreign.lemma.view }
+public struct Vocabulary {
+    private var entries: [Entry]
+    public let version: Version
+    public init(version: Version, entries: [Entry] = []) {
+        self.entries = entries
+        self.version = version
     }
     
+    public struct Version {
+        let major: Int
+        let minor: Int
+        let patch: Int
+    }
 }
 
+#if swift(>=3.0)
+    typealias MutableCollectionProtocol = MutableCollection
 #else
-extension SequenceType where Generator.Element == Entry {
+    typealias MutableCollectionProtocol = MutableCollectionType
+#endif
 
-    /// Entries sorted in alphabetical order.
-	var alphabetical: Vocabulary {
-		return self.sort({ $0.foreign.lemma.view < $1.foreign.lemma.view })
-	}
-
+extension Vocabulary: MutableCollectionProtocol {
+    public typealias Element = Entry
+    public typealias Index = Int
+    public var endIndex: Int {
+        return entries.endIndex
+    }
+    public var startIndex: Int {
+        return entries.startIndex
+    }
+    public subscript(position: Int) -> Entry {
+        get {
+            return entries[position]
+        }
+        set(entry) {
+            entries[position] = entry
+        }
+    }
+    #if swift(>=3.0)
+        public func makeIterator() -> IndexingIterator<[Entry]> {
+            return entries.makeIterator()
+        }
+    #else
+        public func generate() -> IndexingGenerator<[Entry]> {
+            return entries.generate()
+        }
+    #endif
 }
 
-#endif
+extension Vocabulary {
+    var alphabetical: [Entry] {
+        #if swift(>=3.0)
+            return self.sorted { $0.foreign.lemma.view < $1.foreign.lemma.view }
+        #else
+            return self.sort({ $0.foreign.lemma.view < $1.foreign.lemma.view })
+        #endif
+    }
+}
